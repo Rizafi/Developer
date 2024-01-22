6799c5df68acab0dae3c11e26e179c84dd80f4aa
Rizafi <157339236+Rizafi@users.noreply.github.com>
2024-01-22 11:51:10 +0700

    Upload First

---
+++ pages.js
@@ -0,0 +1,55 @@
+function a1() {
+  document.querySelectorAll('a[href^="#home"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
+function a2() {
+  document.querySelectorAll('a[href^="#about"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
+function a3() {
+  document.querySelectorAll('a[href^="#portfolio"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
+function a4() {
+  document.querySelectorAll('a[href^="#project"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
+function a5() {
+  document.querySelectorAll('a[href^="#contact"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+}
---
+++ typed.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#typed", {
+  // Waits 1000ms after typing "First"
+  strings: [
+    "IT Student.",
+    "Graphic Designer.",
+    "Music Composer.",
+    "Content Creator.",
+  ],
+  loop: true,
+  typeSpeed: 30,
+  backSpeed: 25,
+});
